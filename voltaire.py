b82d13e3e7d667e9eb2df579677619315660c28b
Kaoes <cyril.forveille@epita.fr>
2017-02-10 23:34:41 +0700

    commit

--- voltaire.py
+++ voltaire.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 usr_name = input('User name :')
 usr_pss = input('User password :')
@@ -7,6 +8,10 @@ webpage = "https://www.projet-voltaire.fr/"
 
 driver = webdriver.Firefox()
 driver.get(webpage)
+
+
+buttonlog = driver.find_element_by_id("authenticateOption")
+buttonlog.click()
 
 usr_name_box = driver.find_element_by_id("login-username")
 usr_name_box.send_keys(usr_name)
